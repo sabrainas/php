3721baadd472dd68c17e19dea2e65540dea23d9b
sabrina mendonça <sazonass.m@gmail.com>
2023-09-13 10:12:40 -0300

    classes

--- aula01/classes/Pessoa.php
+++ aula01/classes/Pessoa.php
@@ -10,4 +10,36 @@ class Pessoa{
         $this->nome = $nome;
         $this->idade = $idade;
     }
+
+    public function falar($assunto){
+        if($this->falando){
+            echo "{$this->nome} já está falando.\n";
+            return;
+        }
+        echo "{$this->nome} esta falando sobre $assunto.";
+        $this->falando = True;
+    }
+
+    public function parar_falar(){
+        if(!$this->falando){
+            echo "{$this->nome} não está falando\n";
+            return;
+        }
+        echo "{$this->nome} parou de falar\n";
+        $this->falando = False;
+    }
+
+    public function comer($alimento){
+        if($this->falando){
+            echo "{$this->nome} não pode comer falando\n";
+            return;
+        }
+        if($this->comendo){
+            echo "{$this->nome} ja está comendo\n";
+            return;
+        }
+
+        echo "{$this->nome} esta comendo\n";
+        $this->comendo = True;
+    }
 }
--- aula01/index.php
+++ aula01/index.php
@@ -5,5 +5,10 @@ $luiz = new Pessoa('Luiz', 32);
 $joao = new Pessoa('Joao', 25);
 echo $luiz->nome . PHP_EOL; //quebra de linha
 echo $joao->nome . PHP_EOL;
+
+$luiz->falar("Programação orientada a objetos");
+$joao->falar("Animais");
+$luiz->parar_falar();
+$luiz->falar("animais"); 
 //$pessoa1->nome = 'Luiz';
 //echo $pessoa1->nome;
