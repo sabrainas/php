f48efef1bfeec9501f7c1da2a871bc68552537fb
sabrina mendonça <sazonass.m@gmail.com>
2023-09-15 10:52:24 -0300

    aula03

---
+++ aula03/classes/ManipulaArquivo.php
@@ -0,0 +1,21 @@
+<?php
+class ManipulaArquivo{
+    
+    // public function __construct($caminho_arquivo) {
+    //     $this->caminho_arquivo = $caminho_arquivo;
+    //     $this->arquivo = fopen($caminho_arquivo, 'w+');
+    // }
+
+    // public function escrever($string){
+    //     fwrite($this->arquivo, $string > PHP_EOL);
+    // }
+    // public function ler(){
+    //     return fread($this->arquivo, filesize($this->caminho_arquivo));
+    // }
+    // public function __destruct() {
+    //     fclose($this->arquivo);
+    //     echo "{$this->caminho_arquivo} foi fechado";
+    //     unlink($this->caminho_arquivo);
+    // }
+
+}
---
+++ aula03/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'classes/ManipulaArquivo.php';
+
+// $a1 = new ManipulaArquivo('abc.txt');
+// $a1 -> escrever("Ola mundo!");
+// $a1 -> escrever("Escrevendo no arquivo.");
+// echo $a1->ler();
+
